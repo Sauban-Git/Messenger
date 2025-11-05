3b6996c0f2ae46f799591eef4bbd6426581e3c98
Sauban <sauban.git@gmail.com>
2025-11-05 21:10:22 +0530

    init: router and schema

---
+++ server/src/routes/conversation.ts
@@ -0,0 +1,8 @@
+import { Router, type Request, type Response } from "express";
+
+const router = Router();
+router.get("/", (req: Request, res: Response) => {
+  console.log("getting all users.....Blah Blah")
+})
+
+export { router as conversationRouter }
